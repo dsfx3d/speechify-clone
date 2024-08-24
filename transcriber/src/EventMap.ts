50884a1f391499d821e373ae6c036aded181bc89
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-08-24 19:49:09 +0530

    feat: Add SocketEvents to EventMap

--- transcriber/src/EventMap.ts
+++ transcriber/src/EventMap.ts
@@ -1,8 +1,13 @@
 import {IO} from "fp-ts/lib/IO";
 import {Reader} from "fp-ts/lib/Reader";
+import {SocketEvent} from "./SocketEvent";
 import {TranscriberEvent} from "./TranscriberEvent";
 
 export type EventMap = {
+  [SocketEvent.Connect]: IO<void>;
+  [SocketEvent.Disconnect]: IO<void>;
+  [SocketEvent.ConnectionError]: IO<void>;
+  [SocketEvent.Error]: Reader<string, void>;
   [TranscriberEvent.Ready]: IO<void>;
   [TranscriberEvent.FinalTranscript]: Reader<string, void>;
   [TranscriberEvent.PartialTranscript]: Reader<string, void>;
